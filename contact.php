c6b030e17e7da1881fe6b60d09fd2001b7e9ba71
Shemsonex <shemsonex@gmail.com>
2024-01-28 19:29:45 +0200

    Added Contact.php script

--- contact.php
+++ contact.php
@@ -31,7 +31,7 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
-// header('Location: index.html');
+header('Location: index.html');
 
 
 // Function to validate against any email injection attempts
